a256c308b28934ca8a8eb261430579faa7337b1c
juho <blueblack969@naver.com>
2020-10-25 23:55:34 +0900

    add battomNavigation, Fragment bundle

---
+++ app/src/main/java/com/check/corona_prototype/Fragment4.java
@@ -0,0 +1,21 @@
+package com.check.corona_prototype;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment4 extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_4, container, false);
+    }
+}
--- app/src/main/java/com/check/corona_prototype/MainActivity.java
+++ app/src/main/java/com/check/corona_prototype/MainActivity.java
@@ -3,6 +3,7 @@ package com.check.corona_prototype;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,99 +13,151 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.check.corona_prototype.FaceDetection.FaceActivity;
 import com.check.corona_prototype.QR.CreateQR;
 import com.check.corona_prototype.QR.ScanQR;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static com.check.corona_prototype.R.drawable.check_ok;
 
 // 로그인 완료, 메인
 public class MainActivity extends AppCompatActivity {
-    TextView get_id;
-    String id, store;
+    String id, name, store;
     Button btn_picture, btn_qrcode;
     int check_camera = 0, check_qr = 0;
     ImageView imageView1 = null, imageView2 = null;
 
+    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private Fragment1 fragment1 = new Fragment1();
+    private Fragment2 fragment2 = new Fragment2();
+    private Fragment3 fragment3 = new Fragment3();
+    private Fragment4 fragment4 = new Fragment4();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        get_id = findViewById(R.id.get_id);
-        btn_picture = findViewById(R.id.btn_picture);
-        btn_qrcode = findViewById(R.id.btn_qrcode);
-        imageView1 = findViewById(R.id.iv_check1);
-        imageView2 = findViewById(R.id.iv_check2);
+//        btn_picture = findViewById(R.id.btn_picture);
+//        btn_qrcode = findViewById(R.id.btn_qrcode);
+//        imageView1 = findViewById(R.id.iv_check1);
+//        imageView2 = findViewById(R.id.iv_check2);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String name = bundle.getString("name");
+        name = bundle.getString("name");
         id = bundle.getString("id");
         getIntent().getExtras().clear();
 
-        get_id.setText(name + "님 안녕하세요.");
 
-        // 사진버튼
-        btn_picture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FaceActivity.class);
-                intent.putExtra("id", id);
-                startActivityForResult(intent, 1);
-            }
-        });
 
-        // QR코드 버튼
-        btn_qrcode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ScanQR.class);
-                intent.putExtra("id", id);
-                startActivityForResult(intent, 2);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_option, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item)
-    {
-        switch(item.getItemId())
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
         {
-            case R.id.menu1:
-                Intent intent = new Intent(MainActivity.this, CreateQR.class);
-                startActivity(intent);
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.navigation_menu1:
+                        setFrag(0);
+                        break;
+                    case R.id.navigation_menu2:
+                        setFrag(1);
+                        break;
+                    case R.id.navigation_menu3:
+                        setFrag(2);
+                        break;
+                    case R.id.navigation_menu4:
+                        setFrag(3);
+                        break;
+                }
+                return true;
+            }
+        });
+        setFrag(0); // 초기 프래그먼트 지정
+
+
+
+//        // 사진버튼
+//        btn_picture.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, FaceActivity.class);
+//                intent.putExtra("id", id);
+//                startActivityForResult(intent, 1);
+//            }
+//        });
+//
+//        // QR코드 버튼
+//        btn_qrcode.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ScanQR.class);
+//                intent.putExtra("id", id);
+//                startActivityForResult(intent, 2);
+//            }
+//        });
+    }
+
+    // 프레그먼트 교체
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft= fm.beginTransaction();
+        switch (n)
+        {
+            case 0:
+                ft.replace(R.id.container, fragment1);
+                ft.commit();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("name", name);
+                fragment1.setArguments(bundle);
+                break;
+
+            case 1:
+                ft.replace(R.id.container, fragment2);
+                ft.commit();
+                break;
+
+            case 2:
+                ft.replace(R.id.container, fragment3);
+                ft.commit();
+                break;
+
+            case 3:
+                ft.replace(R.id.container, fragment4);
+                ft.commit();
                 break;
         }
-        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1){
-            if (resultCode == RESULT_OK){
-                btn_picture.setEnabled(false);
-                check_camera = 1;
-                imageView1.setImageResource(check_ok);
-            }
-        } else if (requestCode == 2){
-            if (resultCode == RESULT_OK){
-                btn_qrcode.setEnabled(false);
-                check_qr = 1;
-                imageView2.setImageResource(check_ok);
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 1){
+//            if (resultCode == RESULT_OK){
+//                btn_picture.setEnabled(false);
+//                check_camera = 1;
+//                imageView1.setImageResource(check_ok);
+//            }
+//        } else if (requestCode == 2){
+//            if (resultCode == RESULT_OK){
+//                btn_qrcode.setEnabled(false);
+//                check_qr = 1;
+//                imageView2.setImageResource(check_ok);
 
 
 //                해결 포인트 -> store 가져올 것
@@ -112,19 +165,19 @@ public class MainActivity extends AppCompatActivity {
 //                Bundle bundle = intent.getExtras();
 //                store = bundle.getString("store");
 //                Toast.makeText(getApplicationContext(), "Store : " + store, Toast.LENGTH_SHORT).show();
-            }
-        }
-        if (check_camera == 1 && check_qr == 1){
-            Toast.makeText(getApplicationContext(), "인증 완료", Toast.LENGTH_SHORT).show();
-            Handler delayHandler = new Handler();
-            delayHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {}
-            }, 1500);
-            Intent intent = new Intent(this, FinishActivity.class);
-            intent.putExtra("store", store);
-            startActivity(intent);
-            onDestroy();
-        }
-    }
+//            }
+//        }
+//        if (check_camera == 1 && check_qr == 1){
+//            Toast.makeText(getApplicationContext(), "인증 완료", Toast.LENGTH_SHORT).show();
+//            Handler delayHandler = new Handler();
+//            delayHandler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {}
+//            }, 1500);
+//            Intent intent = new Intent(this, FinishActivity.class);
+//            intent.putExtra("store", store);
+//            startActivity(intent);
+//            onDestroy();
+//        }
+//    }
 }
